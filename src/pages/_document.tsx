85e1292edc5ce41d048cbd2037f1434fe672a8c7
thethiago14 <thethiago27@gmail.com>
2022-05-26 13:34:17 -0300

    Remove error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,6 @@ export default class MyDocument extends Document {
                     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;900&display=swap"
                           rel="stylesheet"/>
-                    <link ref="shortcut" href="favicon.png" type="image/png"/>
                 </Head>
                 <body>
                 <Main/>
