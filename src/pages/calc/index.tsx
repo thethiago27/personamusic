2f1946c9529e5b664b73e8dddadd5bad6ea1d0c8
thethiago14 <thethiago27@gmail.com>
2022-05-30 09:51:46 -0300

    fix neural

--- src/pages/calc/index.tsx
+++ src/pages/calc/index.tsx
@@ -1,18 +1,22 @@
-import styles from "./styles.module.scss";
-import Link from "next/link";
 import {useEffect, useState} from "react";
 import Script from "next/script";
-import {motion} from "framer-motion";
 import {GetServerSideProps} from "next";
 import {useRouter} from "next/router";
-import axios from "axios";
 import {api} from "../../services/api";
 import {Loading} from "../../components/Loading";
 import {Result} from "../../components/Result";
+import {checkWindow} from "../../services/user";
 
 interface CalcInterface {
     accessToken: string;
     user: any;
+}
+
+declare global {
+    interface Window {
+        // @ts-ignore
+        Spotify: typeof Spotify;
+    }
 }
 
 export default function Calc({accessToken, user}: CalcInterface) {
@@ -25,6 +29,40 @@ export default function Calc({accessToken, user}: CalcInterface) {
     const [showResult, setShowResult] = useState(false);
 
     useEffect(() => {
+
+        if(checkWindow) {
+
+            // @ts-ignore
+            window.onSpotifyWebPlaybackSDKReady  = () => {
+                const player = new window.Spotify.Player({
+                    name: "The Spotify Quiz",
+                    getOAuthToken: (cb: any) => {
+                        cb(accessToken);
+                    }
+                });
+
+                player.addListener("ready", ({device_id}: any) => {
+                    console.log("Ready with Device ID", device_id);
+                    setPlayer(device_id)
+                });
+
+                player.addListener("player_state_changed", ({
+                                                                track_window: {current_track}
+                                                            }: any) => {
+
+                    console.log('Currently Playing', current_track);
+
+                    current++
+
+                    console.log(current)
+                    setLoading(current === 4 && false)
+                })
+
+                // Connect to the player!
+                player.connect();
+
+            }
+        }
 
         async function playback() {
 
@@ -39,7 +77,6 @@ export default function Calc({accessToken, user}: CalcInterface) {
         }
 
         playback();
-
     }, [player]);
     let current = 1
 
@@ -53,41 +90,6 @@ export default function Calc({accessToken, user}: CalcInterface) {
             <Script
                 src="https://sdk.scdn.co/spotify-player.js"
                 strategy="afterInteractive"
-                onLoad={() => {
-                    // @ts-ignore
-                    window.onSpotifyWebPlaybackSDKReady = () => {
-                        // @ts-ignore
-                        const player = new window.Spotify.Player({
-                            name: "The Spotify Quiz",
-                            getOAuthToken: (cb: any) => {
-                                cb(accessToken);
-                            }
-                        });
-
-                        // Ready
-                        player.addListener("ready", ({device_id}: any) => {
-                            console.log("Ready with Device ID", device_id);
-                            setPlayer(device_id)
-                        });
-
-                        player.addListener("player_state_changed", ({
-                                                                        position,
-                                                                        duration,
-                                                                        track_window: {current_track}
-                                                                    }: any) => {
-
-                            console.log('Currently Playing', current_track);
-
-                            current++
-
-                            console.log(current)
-                            setLoading(current === 4 && false)
-                        })
-
-                        // Connect to the player!
-                        player.connect();
-                    }
-                }}
             />
             {showResult ? (<Result user={user}/>) : <Loading onClick={showResultFunc} loading={loading}/>}
 
